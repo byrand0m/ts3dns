a83754f319366d5ab5be92fbed98cc88dbe07c0c
Can Aydın <51122560+byc4N@users.noreply.github.com>
2019-05-28 17:21:24 +0300

    Add files via upload

---
+++ ayar.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$apikey = 'apikey giriniz';
+$email = 'info@serverbutik.com';
+$domains = array(
+    'domain1.com',
+    'domain2.com',   
+    'domain3.com',
+);  
+
+?>
---
+++ readme.md
@@ -0,0 +1,7 @@
+Türkçe
+
+Gelişmiş Fonksiyonlu Teamspeak DNS Sistemi v2019
+
+Yapımcı ve Yayıncı: www.serverbutik.com / c4N
+
+
